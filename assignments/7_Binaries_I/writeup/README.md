35c54fbbcb46841fd30b2eec7e32490249254590
ryan-ellis <ryan-ellis@users.noreply.github.com>
2019-04-13 23:44:55 -0400

    partial upload

--- assignments/7_Binaries_I/writeup/README.md
+++ assignments/7_Binaries_I/writeup/README.md
@@ -1,12 +1,12 @@
 # Writeup 7 - Binaries I
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: Ryan Ellis
+Section: 0102
 
 I pledge on my honor that I have not given or received any unauthorized
 assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: Ryan Ellis
 
 ## Assignment Writeup
 
--- assignments/7_Binaries_I/writeup/main.c
+++ assignments/7_Binaries_I/writeup/main.c
@@ -1,11 +1,50 @@
 /*
- * Name: *PUT YOUR NAME HERE*
- * Section: *PUT YOUR SECTION NUMBER HERE*
+ * Name: Ryan Ellis
+ * Section: 0102
  *
  * I pledge on my honor that I have not given or received any unauthorized
  * assistance on this assignment or examination.
  *
- * Digital acknowledgement: *PUT YOUR NAME HERE*
+ * Digital acknowledgement: Ryan Ellis
  */
 
 /* your code goes here */
+#include <stdio.h>
+
+int main(){
+	int *rsp;
+	rsp -= 10;
+
+	int *var1, *var2;
+	var1 = 0x1ceb00da;
+	var2 = 0xfeedface;
+
+	int *eax, *esi;
+	char *edi;
+
+	eax = 0xfeedface;
+	esi = eax;
+	edi = "'a = %d\n'"
+	eax = 0;
+	printf(edi, esi);
+
+	eax = 0x1ceb00da;
+	esi = eax;
+	edi = "'a = %d\n'"
+	eax = 0;
+	printf(edi, esi);
+
+	//XOR STUFF
+
+	eax = 0xfeedface;
+	esi = eax;
+	edi = "'a = %d\n'"
+	eax = 0;
+	printf(edi, esi);
+
+	eax = 0x1ceb00da;
+	esi = eax;
+	edi = "'a = %d\n'"
+	eax = 0;
+	printf(edi, esi);
+}
